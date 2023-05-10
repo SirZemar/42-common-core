31ed8f8f651b63d2361d6979269690548498de55
SirZemar <eduardo.w.marinho@gmail.com>
2023-05-10 18:14:39 +0100

    libft: 100% complete bonus

---
+++ projects/01_libft/39_lstadd_back/ft_lstadd_back.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jose-ero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 12:28:12 by jose-ero          #+#    #+#             */
+/*   Updated: 2023/05/09 18:25:19 by jose-ero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!lst)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	last = ft_lstlast(*lst);
+	if (!last)
+		return ;
+	last->next = new;
+}
